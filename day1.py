d5fc4a23a85d2fb4ac2230bc13962c2fab51032e
jbm-painourt <jbmanimes@gmail.com>
2026-07-18 09:07:32 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,8 @@
+
+print("MY AI JOURNEY STARTS TODAY")
+print(f"Name: buddy")
+print(f"Background: ece")
+print(f"Goal: ai architect")
+print(f"Daily commitment: 10 hours")
+print(f"Start date: july 18th")
+
